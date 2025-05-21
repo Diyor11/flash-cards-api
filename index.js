41348460e27db85d723ffdff58e01f6af8003ce3
Diyor <77630836+Diyor11@users.noreply.github.com>
2025-05-21 11:29:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,62 @@ const Word = require('./modules/Word');
 
 
 app.get('/api/v', (req, res) => res.send({succces: true, message: 'Server working', v: '1.0.0'}))
-app.use('/api', require('./router/word'))
+const Words =  require('../modules/Word')
+
+api.get('/api', async(req, res) => {
+    try {
+        const words = await Words.find({        })
+        res.send(words)
+    } catch (e) {
+        res.status(500).send({message: 'Unexpected server error'})
+    }
+})
+
+app.get('/api/due', async(req, res) => {
+  try {
+    const words = await Words.find({
+      lastSeen: { $lt: new Date() }
+    }) // filter words which lastSeen < Date.now()
+    res.send(words)
+  } catch (e) {
+      res.status(500).send({message: 'Unexpected server error'})
+  }
+})
+
+const intervlalMs = [
+  1000 * 90,
+  1000 * 60 * 60 * 2,
+  1000 * 60 * 60 * 12,
+  1000 * 60 * 60 * 24
+] 
+app.post('/api/update-card', async (req, res) => {
+    const { wordId, success } = req.body;
+    
+    try {
+        // Find the word by wordId
+        const word = await Words.findOne({ wordId });
+
+        if (!word) {
+            return res.status(404).send({ message: 'Word not found' });
+        }
+
+        // Increment stage if success is true
+        if (success) {
+            word.reviewDate = new Date(Date.now() + intervlalMs[words.stage])
+            word.stage = word.stage  + 1; // Initialize to 0 if stage is null
+          } else {
+            word.stage = 1; // Initialize to 0 if stage is null
+            word.reviewDate = new Date(Date.now() + intervlalMs[0])
+        }
+
+        // Save the updated word
+        await word.save();
+
+        res.status(200).send({ message: 'Word updated successfully', word });
+    } catch (e) {
+        res.status(500).send({ message: 'Unexpected server error' });
+    }
+});
 
 
 const port = process.env.PORT || 8080
